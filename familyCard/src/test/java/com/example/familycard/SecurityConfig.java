64ac1c3afea85ee27584196bcca623a516a83f18
TsitouhRanjafy <radohery09@gmail.com>
2026-05-10 19:19:22 +0300

    setup a basic authentification

---
+++ familyCard/src/test/java/com/example/familycard/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.familycard;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+// @Configuration: tells Spring to use this class to configure Spring and Spring Boot itself.
+// Any Beans specified in this class will now be available to Spring's Auto Configuration engine.
+// for more detail, in starting of spring, spring scan this class and add all config in his container IoC
+@Configuration
+public class SecurityConfig {
+
+    // with Bean, we use the SecurityFilterChain of spring security
+    // without Bean, it's a normal object (ordinary object)
+    // By default, spring has already this method SecurityFilterChain (the name is not important)
+    // But, we override this default method, spring use this
+    // We can specify the primary method with @Primary when it's more method with same return's
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/cashcards/**")
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .build();
+
+    }
+
+    @Bean
+    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder();
+        UserDetails sarah = users
+                .username("sarah1")
+                .password(passwordEncoder.encode("abc123"))
+                .roles("CARD-OWNER") // No roles for now
+                .build();
+        return new InMemoryUserDetailsManager(sarah);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
